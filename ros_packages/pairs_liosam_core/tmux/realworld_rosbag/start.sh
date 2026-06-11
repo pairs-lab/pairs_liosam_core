313c4a16def3eaf7d83ae705670f758162d642ce
thanhnguyencanh <canhthanhlt@gmail.com>
2026-06-11 22:51:05 +0200

    tmux: replace tmuxinator sessions with plain tmux scripts
    - start scripts now build the session with plain tmux (new-session,
     split-window, send-keys); tmuxinator and /etc/ctu-pairs/tmux.conf are
     no longer required
    - kill.sh stops the session gracefully; a pre-typed kill window is added

--- ros_packages/pairs_liosam_core/tmux/realworld_rosbag/start.sh
+++ ros_packages/pairs_liosam_core/tmux/realworld_rosbag/start.sh
@@ -1,27 +1,65 @@
 #!/bin/bash
+# Plain-tmux session script (one window per section below; panes are split and
+# their commands typed via send-keys). Edit the send-keys lines to change what
+# runs in each pane; ./kill.sh stops everything.
 
-# Absolute path to this script. /home/user/bin/foo.sh
-SCRIPT=$(readlink -f $0)
-# Absolute path this script is in. /home/user/bin
-SCRIPTPATH=`dirname $SCRIPT`
-cd "$SCRIPTPATH"
+SESSION_NAME=liosam_rosbag
 
-export TMUX_SESSION_NAME=liosam_rosbag
-export TMUX_SOCKET_NAME=pairs
+# absolute path to this script's directory; every pane starts here
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
 
-# start tmuxinator
-tmuxinator start -p ./session.yml
+# commands executed first in every pane
+PRE_WINDOW='export UAV_NAME=uav25; export RUN_TYPE=realworld; export UAV_TYPE=x500; export SENSORS=ouster'
+SETUP="cd $SCRIPTPATH; $PRE_WINDOW"
 
-# if we are not in tmux
-if [ -z $TMUX ]; then
-
-  # just attach to the session
-  tmux -L $TMUX_SOCKET_NAME a -t $TMUX_SESSION_NAME
-
-# if we are in tmux
-else
-
-  # switch to the newly-started session
-  tmux detach-client -E "tmux -L $TMUX_SOCKET_NAME a -t $TMUX_SESSION_NAME" 
-
+if [ -n "$TMUX" ]; then
+  echo "Already inside tmux, detach first."
+  exit 1
 fi
+
+if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
+  echo "Session $SESSION_NAME already exists; attach with 'tmux a -t $SESSION_NAME' or stop it with ./kill.sh."
+  exit 1
+fi
+
+# ---------------- window: roscore ----------------
+read W_roscore P <<< "$(tmux new-session -d -s "$SESSION_NAME" -n "roscore" -x 250 -y 50 -P -F '#{window_id} #{pane_id}')"
+tmux send-keys -t "$P" "$SETUP; "'roscore' Enter
+tmux select-layout -t "$W_roscore" tiled
+
+# ---------------- window: rosbag ----------------
+read W_rosbag P <<< "$(tmux new-window -t "$SESSION_NAME" -n "rosbag" -P -F '#{window_id} #{pane_id}')"
+tmux send-keys -t "$P" "$SETUP; "'waitForRos; sleep 5; rosbag play `rospack find pairs_liosam_core`/bag_files/camp.bag --clock -r 3' Enter
+tmux select-layout -t "$W_rosbag" tiled
+
+# ---------------- window: slam ----------------
+read W_slam P <<< "$(tmux new-window -t "$SESSION_NAME" -n "slam" -P -F '#{window_id} #{pane_id}')"
+tmux send-keys -t "$P" "$SETUP; "'waitForRos; rosparam set use_sim_time true; roslaunch pairs_liosam_core slam_pipeline.launch rosbag_replay:=true IMU_TYPE:=ouster OUSTER_TYPE:=OS1-16' Enter
+tmux select-layout -t "$W_slam" tiled
+
+# ---------------- window: tf ----------------
+read W_tf P <<< "$(tmux new-window -t "$SESSION_NAME" -n "tf" -P -F '#{window_id} #{pane_id}')"
+tmux send-keys -t "$P" "$SETUP; "'waitForRos; rosrun tf2_ros static_transform_publisher 0 0 0 0 0 0 $UAV_NAME/fcu $UAV_NAME/os_sensor' Enter
+tmux select-layout -t "$W_tf" tiled
+
+# ---------------- window: rviz ----------------
+read W_rviz P <<< "$(tmux new-window -t "$SESSION_NAME" -n "rviz" -P -F '#{window_id} #{pane_id}')"
+tmux send-keys -t "$P" "$SETUP; "'waitForRos; roslaunch pairs_liosam_core rviz.launch rviz_file:=rosbag.rviz' Enter
+P=$(tmux split-window -t "$W_rviz" -P -F '#{pane_id}')
+tmux select-layout -t "$W_rviz" tiled
+tmux send-keys -t "$P" "$SETUP; "'waitForTime; roslaunch pairs_rviz_plugins load_robot.launch' Enter
+P=$(tmux split-window -t "$W_rviz" -P -F '#{pane_id}')
+tmux select-layout -t "$W_rviz" tiled
+tmux send-keys -t "$P" "$SETUP; "'waitForTime; waitForControl; roslaunch pairs_rviz_plugins rviz_interface.launch' Enter
+tmux select-layout -t "$W_rviz" tiled
+
+# ---------------- window: kill (press enter inside to stop the session) ----------------
+read W_kill P <<< "$(tmux new-window -t "$SESSION_NAME" -n "kill" -P -F '#{window_id} #{pane_id}')"
+tmux send-keys -t "$P" "$SCRIPTPATH/kill.sh"
+
+# mouse support (select panes / scroll with the mouse)
+tmux set-option -t "$SESSION_NAME" mouse on
+
+tmux select-window -t "$W_slam"
+tmux -2 attach-session -t "$SESSION_NAME"
